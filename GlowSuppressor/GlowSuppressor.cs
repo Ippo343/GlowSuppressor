f515fdb0d077e47b16d2fd31fb03086ff8e1e788
Ippo343 <m.ippolito@outlook.com>
2014-07-12 20:26:13 +0200

    Initial release
    ... and probably last.

---
+++ GlowSuppressor/GlowSuppressor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace GlowSuppressor
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class GlowSuppressor : MonoBehaviour
+    {
+        void Update()
+        {
+            if (FlightGlobals.ActiveVessel != null)
+                SuppressGlow(FlightGlobals.ActiveVessel.rootPart);
+        }
+
+        private static void SuppressGlow(Part part)
+        {
+            part.SetHighlightType(Part.HighlightType.Disabled);
+            part.SetHighlight(false);
+
+            // Reset the glow for all the child parts
+            foreach (Part child in part.children)
+                GlowSuppressor.SuppressGlow(child);
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,8 @@ GlowSuppressor
 ==============
 
 KSP Plugin that prevents parts from glowing
+
+This plugin will suppress the parts' glow at each frame, thus preventing the green glow from appearing when you hover the mouse on it.
+It can be useful for filming short videos or cinematics.
+
+The code is in the public domain.
