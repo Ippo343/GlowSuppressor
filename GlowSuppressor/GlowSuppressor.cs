360cd56d70bca027c71a4f087734bc5374c1a65f
Michele <m.ippolito@outlook.com>
2014-12-16 18:22:07 +0100

    0.90 update

--- GlowSuppressor/GlowSuppressor.cs
+++ GlowSuppressor/GlowSuppressor.cs
@@ -9,24 +9,12 @@ namespace GlowSuppressor
     [KSPAddon(KSPAddon.Startup.Flight, false)]
     public class GlowSuppressor : MonoBehaviour
     {
-        void Update()
+        void LateUpdate()
         {
             foreach (Vessel v in FlightGlobals.Vessels)
             {
-                if (v != null)
-                    SuppressGlow(FlightGlobals.ActiveVessel.rootPart);
+                v.rootPart.SetHighlight(false, true);
             }
         }
-
-        private static void SuppressGlow(Part part)
-        {
-            part.SetHighlightType(Part.HighlightType.Disabled);
-            part.SetHighlight(false);
-
-            // Reset the glow for all the child parts
-            foreach (Part child in part.children)
-                GlowSuppressor.SuppressGlow(child);
-        }
-
     }
 }
